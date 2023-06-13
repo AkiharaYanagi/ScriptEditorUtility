bde1f18154db937451ef2406196818ab009b6a40
YANAGI KEIJIROU <yanagikeijiro@hogmail.com>
2023-06-13 11:42:20 +0900

    Ctrl_Compend移植開始

---
+++ Ctrl_Chara/Compend/RB_Tool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ctrl_Chara.Compend
+{
+	public partial class RB_Tool : Component
+	{
+		public RB_Tool ()
+		{
+			InitializeComponent ();
+		}
+
+		public RB_Tool ( IContainer container )
+		{
+			container.Add ( this );
+
+			InitializeComponent ();
+		}
+	}
+}
